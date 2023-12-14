f7893e9aa12d8390465d4451fa65f67f603f3646
hasan-deriv <abuhasan@deriv.com>
2023-12-14 15:25:33 +0800

    feat(index.tsx): publish action sheet 6

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 import ActionSheetHeader from './header'
 
-export interface ActionSheetType {
-  Header: React.FC<typeof ActionSheetHeader>
+export type ActionSheetType = React.FunctionComponent & {
+  Header: React.FC
 }
 
-export const ActionSheet: ActionSheetType = ({
-  children,
-}: {
-  children: ReactNode
-}) => {
-  return <>{children}</>
-}
+export const ActionSheet: ActionSheetType = ({ children }): JSX.Element => <>{children}</>;
 
 ActionSheet.Header = ActionSheetHeader
 
